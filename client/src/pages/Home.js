eaf0cbeea3c125c3489e45886fc0774798e16571
ad0rshcodes <adarsharma.me@gmail.com>
2024-04-13 10:59:29 -0500

    landing page add

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="home-container">
+      <h1>Welcome to Smart Insurance Platform</h1>
+      <p>Your one-stop solution for decentralized insurance products.</p>
+      <div className="navigation-links">
+        <Link to="/signup">Sign Up</Link>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,7 +1,25 @@
-import React from "react";
+// import React from "react";
+// import { useAuth } from "@propelauth/react";
 
-function Home() {
-  return <div>Sign Up Page</div>;
-}
+// function SignUp() {
+//   const { authInfo, isAuthenticating } = useAuth();
 
-export default Home;
+//   if (isAuthenticating) {
+//     return <div>Loading...</div>;
+//   }
+
+//   if (authInfo) {
+//     // User is already authenticated
+//     return <div>You are already signed up and logged in!</div>;
+//   }
+
+//   // User is not authenticated, show the sign-up form
+//   return (
+//     <div>
+//       {/* Your sign-up form or a link to PropelAuth's hosted sign-up page */}
+//       <a href="https://your-auth-url.propelauth.com/signup">Sign Up</a>
+//     </div>
+//   );
+// }
+
+// export default SignUp;
